b6295c78876c14f0a7dfac16bc2d480d51934322
Mike149x <mikeismike148@gmail.com>
2021-07-17 08:25:08 +1200

    Video 5 Done

--- SuperStockTrader2/Scenes/Camera.gd
+++ SuperStockTrader2/Scenes/Camera.gd
@@ -1,16 +1,22 @@
 extends Camera
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var player_follow = get_parent().get_node("Player")
 
+var random_jitter = 0
+func get_camera_shake(delta):
+	if (Input.is_mouse_button_pressed(BUTTON_LEFT)):
+		random_jitter += 0.05 * delta
+	else:
+		random_jitter -= 0.5 * delta
+		if random_jitter < 0:
+			random_jitter = 0
+	return Vector2(rand_range(-random_jitter, random_jitter), rand_range(-random_jitter, random_jitter))
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var camera_shake = get_camera_shake(delta)
+	var camera_pos = Vector3.ZERO
+	camera_pos.x = player_follow.translation.x + camera_shake.x
+	camera_pos.z = player_follow.translation.z + camera_shake.y
+	camera_pos.y = get_translation().y
+	set_translation(camera_pos)
