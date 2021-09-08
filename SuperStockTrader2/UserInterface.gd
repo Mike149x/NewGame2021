59df05641a88f8983d5891a53a8451a2a9df33a3
Mike149x <mikeismike148@gmail.com>
2021-09-09 10:40:20 +1200

    to be reverted

--- SuperStockTrader2/UserInterface.gd
+++ SuperStockTrader2/UserInterface.gd
@@ -2,15 +2,17 @@ extends Control
 
 
 func _ready():
-	$HealthBar.max_value = PlayerStats.health_max
-	$AmmoBar.max_value = PlayerStats.ammo_max
+#	$HealthBar.max_value = PlayerStats.health_max
+#	$AmmoBar.max_value = PlayerStats.ammo_max
+	pass
 	
 
 func _process(delta):
-	$HealthBar.value = PlayerStats.get_health()
-	$AmmoCount.text = "x " + PlayerStats.get_ammo()
-	$LifeCounter.text = PlayerStats.get_lives()
-	$AmmoBar.value = int(PlayerStats.get_ammo())
-	
+#	$HealthBar.value = PlayerStats.get_health()
+#	$AmmoCount.text = "x " + PlayerStats.get_ammo()
+#	$LifeCounter.text = PlayerStats.get_lives()
+#	$AmmoBar.value = int(PlayerStats.get_ammo())
+	pass
+#
 
 
