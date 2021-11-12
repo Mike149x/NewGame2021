a0540b811bfde976ac1794d6ea686748828ceecb
Mike149x <mikeismike148@gmail.com>
2021-11-12 15:00:55 +1300

    Level 3 and Menu Screen

---
+++ SuperStockTrader2/Scenes/Menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+
+func _ready():
+	$VBoxContainer/StartButton.grab_focus()
+
+
+
+func _on_StartButton_pressed():
+	get_tree().change_scene("res://Scenes/TestLevel1.tscn")
+
+
+func _on_OptionsButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
